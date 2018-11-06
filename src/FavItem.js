a88ee8ac047013761a3837da49f89c31315f4d8e
Anya <okokanya@gmail.com>
2018-11-06 17:13:59 +0300

    imgs

--- src/FavItem.js
+++ src/FavItem.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import products from "./data/products.json";
 import apiConfig from "./apiConfig.js";
 
-const FavItem = ({ title, image, brand }) => (
+const FavItem = ({ title, pick, brand, price }) => (
   <a className="item-list__item-card item" href="product-card-desktop.html">
-    <div className="item-pic">
+    <div className="item-pic-1">
       <img
-        className="item-pic-2"
-        src="{image}"
+        className="item-pic"
+        src={pick}
         alt=""
       />
       <div className="product-catalogue__product_favorite">
@@ -21,7 +21,7 @@ const FavItem = ({ title, image, brand }) => (
       <p className="item-producer">
         Производитель: <span className="producer">{brand}</span>
       </p>
-      <p className="item-price">23 150</p>
+      <p className="item-price">{price}</p>
       <div className="sizes">
         <p className="sizes__title">Размеры в наличии:</p>
         <p className="sizes__avalible">36, 37, 38, 39, 40, 41, 42</p>
